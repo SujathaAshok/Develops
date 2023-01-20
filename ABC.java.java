009cf693fb6120997919c383df1f3c3ae32b4bfc
SujathaAshok <sujathakolli193@gmail.com>
2023-01-20 19:17:26 +0530

    added new ABC java file

---
+++ ABC.java.java
@@ -0,0 +1,17 @@
+package Firstpractice;
+
+public class ABC {
+	public static void main (String[]args) {
+		int a=10;
+		int b =15;
+		int c=(a+b) *(a-b);
+		System.out.println((a+c) +(a-b));
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/c);
+		System.out.println(a%b);
+		
+	}
+
+}
